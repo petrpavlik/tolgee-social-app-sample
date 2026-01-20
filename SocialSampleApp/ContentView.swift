70eebbc12de094aa918f0af0673e3c6209d11fb2
Petr Pavlik <petrpavlik@me.com>
2026-01-20 20:59:44 +0100

    Initial Commit

---
+++ SocialSampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SocialSampleApp
+//
+//  Created by Petr Pavlik on 20.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SocialSampleApp/SocialSampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialSampleAppApp.swift
+//  SocialSampleApp
+//
+//  Created by Petr Pavlik on 20.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SocialSampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
